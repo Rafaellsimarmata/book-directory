12fafbd3468251f166923b14263549d655ffa608
rafael <simarmatarafael11@gmail.com>
2023-02-07 11:35:45 +0700

    final commit3

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host:  process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'book-directory' ,
+  host: process.env.MYSQLHOST || 'localhost',
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || '',
+  database: process.env.MYSQLDATABASE || 'book-directory',
 });
 
 module.exports = db;
